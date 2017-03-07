5fc7631aaccf5fffde02ca85bb8f2fd16268cc9e
Golcondas <754931220@qq.com>
2017-03-08 00:51:09 +0800

    #restful wcf学习

---
+++ SandwichServices/SandwichServices/CostService.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SandwichServices
+{
+    [ServiceContract(Namespace = "SandwichServices")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class CostService
+    {
+        //来源 http://www.cnblogs.com/jfzhu/p/4041638.html 如何创建一个AJAX-Enabled WCF Service
+
+        // 要使用 HTTP GET，请添加 [WebGet] 特性。(默认 ResponseFormat 为 WebMessageFormat.Json)
+        // 要创建返回 XML 的操作，
+        //     请添加 [WebGet(ResponseFormat=WebMessageFormat.Xml)]，
+        //     并在操作正文中包括以下行:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [OperationContract]
+        public void DoWork()
+        {
+            // 在此处添加操作实现
+            return;
+        }
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Test/CostOfSandwiches", ResponseFormat = WebMessageFormat.Json)]
+        [DescriptionAttribute("1.25*quantity")]
+        // 在此处添加更多操作并使用 [OperationContract] 标记它们
+        public double CostOfSandwiches(int quantity) 
+        {
+            return 1.25 * quantity;
+        }
+    }
+}
---
+++ SandwichServices/TestWcfIIS/WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TestWcfIIS.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
