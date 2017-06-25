cd1288662c7769e3ab775322d4db08a52a458ccd
Golcondas <754931220@qq.com>
2017-06-25 20:46:24 +0800

    #add Foo 方法

--- 递归/递归/Program.cs
+++ 递归/递归/Program.cs
@@ -10,6 +10,24 @@ namespace 递归
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Foo(9));
+            Console.ReadKey();
+        }
+
+        public static int Foo(int n) 
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+            else if (n > 0 && n <= 2)
+            {
+                return 1;
+            }
+            else 
+            {
+                return Foo(n - 1) + Foo(n-2);
+            }
         }
     }
 }
