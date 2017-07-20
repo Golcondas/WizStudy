e7b206d44f20bc5e4b4688d368124056adc190b8
Golcondas <754931220@qq.com>
2017-07-20 23:44:05 +0800

    #add WaitCallback 线程回调

--- 多线程/多线程2/Program.cs
+++ 多线程/多线程2/Program.cs
@@ -9,54 +9,33 @@ namespace 多线程2
 {
     class Program
     {
-        const int repetitions = 100;
-        static void Main(string[] ages) 
+        const int Repetitions = 100;
+        static int index_thread = 0;
+        static int index_main = 0;
+        static void Main(string[] args)
         {
-            ThreadStart threadStart = DoWork;
-            Thread thread = new Thread(threadStart);
-            thread.Start();
-            thread.Join();
-            for (int i = 0; i < repetitions; i++)
+            WaitCallback waitCallback = DoWork;
+            ThreadPool.QueueUserWorkItem(waitCallback, "+");
+            for (int i = 0; i < Repetitions; i++)
             {
+                index_main++;
                 Console.Write("-");
             }
-            Console.WriteLine("主线程跑结束了");
+            Console.WriteLine(string.Format("\nindex_thread:{0}", index_thread));
+            Console.WriteLine(string.Format("index_main:{0}", index_main));
             Console.ReadKey();
         }
-        static void DoWork() 
+        static void DoWork(object ob)
         {
-            for (int i = 0; i < repetitions; i++)
+            for (int count = 0; count < Repetitions; count++)
             {
-                Console.Write("+");
+                index_thread++;
+                Console.Write(ob);
+                if (count == Repetitions - 1)
+                {
+                    Console.Write("我创建的线程执行完成了.....................................\n");
+                }
             }
         }
-        //const int Repetitions = 100;
-        //static int index_thread = 0;
-        //static int index_main = 0;
-        //static void Main(string[] args)
-        //{
-        //    WaitCallback waitCallback = DoWork;
-        //    ThreadPool.QueueUserWorkItem(waitCallback,"+");
-        //    for (int i = 0; i < Repetitions; i++)
-        //    {
-        //        index_main++;
-        //        Console.Write("-");
-        //    }
-        //    Console.WriteLine(string.Format("\nindex_thread:{0}", index_thread));
-        //    Console.WriteLine(string.Format("index_main:{0}", index_main));
-        //    Console.ReadKey();
-        //}
-        //static void DoWork(object ob)
-        //{
-        //    for (int count = 0; count < Repetitions; count++)
-        //    {
-        //        index_thread++;
-        //        Console.Write(ob);
-        //        if (count == Repetitions - 1)
-        //        {
-        //            Console.Write("我创建的线程执行完成了.....................................\n");
-        //        }
-        //    }
-        //}
     }
 }
